fcff46300fb76842e1e2ae52ea6360cc9bafd989
Aaron Hunter <122066290+Loomd@users.noreply.github.com>
2023-03-26 00:49:34 -0700

    Rename

---
+++ CarRental.cpp
@@ -0,0 +1,13 @@
+// Aaron Hunter
+// Unit 6 Programming Assignment 3 - Car Rental.
+// Write a program to calculate car rental cost for any number of customers.
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main () {
+
+
+    return 0;
+}
