aaff64b743955b28538980af8003d974b5728b45
Loomd <ahunter231@mail.suscc.edu>
2023-04-09 02:37:33 -0500

    updated. done

--- Age in Seconds.cpp
+++ Age in Seconds.cpp
@@ -1,32 +1,39 @@
 //Aaron Hunter
 //Unit 6 Programming Assignment 9 - Age in Seconds.
-//Write a program that accepts a person’s s name and age in years, then prints the number of seconds the person has lived.
-//Use a posttest loop to repeat this process until the user indicates there are no more cases to process
-//Use another posttest loop to put an error trap around the age entry to be sure the user enters an age that is at least 0.
-#include<iostream>
-#include<iomanip>
-#include<string>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+
 using namespace std;
 
-const double SEC_IN_YEAR = 365*24*60*60;
+const long double SEC_IN_YEAR = 365.0 * 24 * 60 * 60;
 
-int main(){
-
-    string name;
+int main() {
+    string name = "";
     int age;
-    double seconds;
-    char repeatProgramIsEqualTo;
+    long double seconds;
+    char repeatProgramIsEqualTo = 'Y';
 
     do {
-        cout << "Welcome to 'Age in Seconds' please enter the desired first and last name: ";
+        cout << "Please enter the person's first and last name: ";
         getline(cin, name);
-        cin.ignore(100,'\n');
+        cin.ignore();
+
         do {
-            cout << "\nPlease enter desired years to be converted into seconds: ";
+            cout << "\nPlease enter the person's age in years (must be 0 or greater): ";
             cin >> age;
-            } while(!(cin << age) || age < 0);
-            cout << "\nWould you like to run another iteration of the program? (Y/N): ";
-            cin >> repeatProgramIsEqualTo;
-            toupper(repeatProgramIsEqualTo);
-        } while(repeatProgramIsEqualTo == 'y' || repeatProgramIsEqualTo == 'Y');
+        } while (age < 0);
+
+        seconds = age * SEC_IN_YEAR;
+        cout << "\n" << name << " has lived approximately " << fixed << setprecision(0) << seconds << " seconds.\n\n";
+
+        cout << "Do you want to run the program again? (Y/N): ";
+        cin >> repeatProgramIsEqualTo;
+        repeatProgramIsEqualTo = toupper(repeatProgramIsEqualTo);
+
+        cin.ignore();
+    } while (repeatProgramIsEqualTo == 'Y');
+
+    return 0;
 }
