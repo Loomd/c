20cc406a6c1f9e7c46c888fcb4625efd5887ad4b
Loomd <ahunter231@mail.suscc.edu>
2023-04-11 08:23:35 -0500

    update.

--- grosspay.cpp
+++ grosspay.cpp
@@ -7,17 +7,54 @@ This method should use a single-sided decision to add(+) over-time-pay equal(==)
 #include<iostream>
 using namespace std;
 
-void getHoursWorked(double& hoursWorked) {
-    cout << "\nEnter Hours worked (enter 0 if you wish to exit): ";
+void getHoursWorked(double &, int);
+void getHourlyPay(double &);
+double calculateGrossPay(double, double);
+void calcOvertimePay(double &, double, double);
+
+int main(){
+
+    double hoursWorked, hourlyPay, grossPay, overtimePay = 0;
+    int employee = 1;
+
+    do {
+
+        getHoursWorked(hoursWorked, employee);
+
+        if (hoursWorked == 0) {
+            break;
+        }
+
+        getHourlyPay(hourlyPay);
+        grossPay = calculateGrossPay(hoursWorked, hourlyPay);
+        calcOvertimePay(overtimePay, hourlyPay, hoursWorked);
+
+        cout << "\nHours Worked: " << hoursWorked << " hrs";
+        cout << "\nHourly Pay: $" << hourlyPay;
+        cout << "\nGross Pay: $" << grossPay;
+        cout << "\nOvertime Pay: $" << overtimePay << endl;
+
+        employee++;
+
+  }while (true);
+
+    system("pause");
+    return 0;
+ }
+
+void getHoursWorked(double& hoursWorked, int employee)
+{
+    cout << "\nEnter Hours worked for employee " << employee << " (enter 0 if you wish to exit): ";
     while(!(cin >> hoursWorked) || hoursWorked < 0 ){
-        cout << "Error, value entered was less than 0. Enter a value greater then 0: ";
+        cout << "\nError, value entered was less than 0. Enter a value greater then 0: ";
         cin.clear();
         cin.ignore(100,'\n');
         continue;
     }
 }
 
-void getHourlyPay(double& hourlyPay){
+void getHourlyPay(double& hourlyPay)
+{
     cout << "\nEnter hourly pay: ";
     while(!(cin >> hourlyPay) || hourlyPay < 0){
         cout << "\nError, value entered was less then 0. Enter a value greater then 0: ";
@@ -27,30 +64,15 @@ void getHourlyPay(double& hourlyPay){
     }
 }
 
-double calculateGrossPay(double& grossPay, double hoursWorked, double hourlyPay){
-    grossPay = hoursWorked * hourlyPay;
-    return grossPay;
-}
-//method should use a single-sided decision to add(+) over-time-pay equal(==) to half(.5) of the hourly pay-rate multiplied(*) by hours-worked over(/) 40
-void calcOvertimePay(double& overtimePay, double hourlyPay, double hoursWorked){
+ void calcOvertimePay(double& overtimePay, double hourlyPay, double hoursWorked)
+ {
     if(hoursWorked > 40){
         overtimePay = (hoursWorked - 40) * hourlyPay * 0.5;
     }
 }
 
-int main(){
-    double hoursWorked, hourlyPay; 
-    double grossPay = 0, overtimePay = 0;
-
-    getHoursWorked(hoursWorked);
-    getHourlyPay(hourlyPay);
-    calculateGrossPay(grossPay, hoursWorked, hourlyPay);
-    calcOvertimePay(overtimePay, hourlyPay, hoursWorked);
-
-    cout << "\nHours Worked: " << hoursWorked;
-    cout << "\nHourly Pay: " << hourlyPay;
-    cout << "\nGross Pay: " << grossPay;
-    cout << "\nOvertime Pay: " << overtimePay;
-
-    return 0;
- }
+double calculateGrossPay(double hourlyPay, double hoursWorked)
+{
+    double grossPay = hoursWorked * hourlyPay;
+    return grossPay;
+}
