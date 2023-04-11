0a76d8eaa7d3b2169944ca955605a6ba230088e5
Loomd <ahunter231@mail.suscc.edu>
2023-04-11 06:16:23 -0500

    unit 7 ass 1

---
+++ grosspay.cpp
@@ -0,0 +1,14 @@
+//Aaron Hunter.
+//Unit 7 Programming Assignment 1 - Gross Pay.
+//Write a C++ program to calculate gross pay for any number of employees until a 0 is entered for the number of hours worked.
+//Also calculate and display the total gross pay.
+/*Method to get hours worked (double type) from the user. Use an error trap to ensure the value entered is not negative.
+Test this at least once.
+Method to get hourly pay (double type) from the user. Use an error trap to ensure the value entered is greater than 0.
+Test this at least once.
+Method to calculate the gross pay.
+This method should use a single-sided decision to add(+) over-time-pay equal(==) to half(.5) of the hourly pay-rate multiplied(*) by hours-worked over(/) 40.*/
+#include<iostream>
+using namespace std;
+
+int main(){ }
